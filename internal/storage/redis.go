e43f53930264f4582c3f912e30549c282011e44c
dmsRosa6 <dms.rosa6@gmail.com>
2025-07-23 14:38:44 +0000

    storage part done

--- internal/storage/redis.go
+++ internal/storage/redis.go
@@ -19,10 +19,12 @@ func NewRedisStorage(addr string, expiration time.Duration) *RedisStorage {
 	return &RedisStorage{client: client, expiration: expiration}
 }
 
-func (redis *RedisStorage) Set(ctx context.Context, key, value string) error {
-	return redis.client.Set(ctx, key, value)
-}
+func (redis *RedisStorage) Set(ctx context.Context, key string, value interface{}) error {
+	//TODO:check what that string may or may not return
+	_, err := redis.client.Set(ctx, key, value, redis.expiration).Result()
 
+	return err
+}
 
 func (redis *RedisStorage) Get(ctx context.Context, key string) (string, error) {
 	return redis.client.Get(ctx, key).Result()
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,19 +1,64 @@
 package storage
 
-type InMemoryStorage struct {
-	mu    sync.RWMutex
-	store map[string]string
+import (
+	"container/list"
+	"context"
+	"fmt"
+	"sync"
+)
+
+type MapEntry struct {
+	key   string
+	value string
 }
 
+type InMemoryStorage struct {
+	mu      sync.RWMutex
+	store   map[string]*list.Element
+	lru     *list.List
+	maxSize int
+}
 
-func NewInMemoryStorage(expiration time.Duration) *InMemoryStorage{
-	return &InMemoryStorage{store: make(map[string)string}
+func NewInMemoryStorage(size int) *InMemoryStorage {
+	return &InMemoryStorage{
+		store:   make(map[string]*list.Element, size),
+		maxSize: size,
+		lru:     list.New(),
+	}
 }
 
 func (mem *InMemoryStorage) Set(ctx context.Context, key, value string) error {
+	mem.mu.Lock()
+	defer mem.mu.Unlock()
 
+	if elem, ok := mem.store[key]; ok {
+		elem.Value = MapEntry{key: key, value: value}
+		mem.lru.MoveToFront(elem)
+		return nil
+	}
+
+	if len(mem.store) >= mem.maxSize {
+		back := mem.lru.Back()
+		if back != nil {
+			mem.lru.Remove(back)
+			delete(mem.store, back.Value.(MapEntry).key)
+		}
+	}
+
+	elem := mem.lru.PushFront(MapEntry{key: key, value: value})
+	mem.store[key] = elem
+	return nil
 }
 
-func (mem *InMemoryStorage) Get(ctx context.Context, key string) (string, error){
+func (mem *InMemoryStorage) Get(ctx context.Context, key string) (string, error) {
+	mem.mu.Lock()
+	defer mem.mu.Unlock()
 
+	elem, ok := mem.store[key]
+	if !ok {
+		return "", fmt.Errorf("key not found")
+	}
+
+	mem.lru.MoveToFront(elem)
+	return elem.Value.(MapEntry).value, nil
 }
